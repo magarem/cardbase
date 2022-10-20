1070b1485f91ca943e4f4fc62754e779256ff30c
marcelo <contato@magaweb.com.br>
2022-10-20 15:52:59 -0300

    pwa fixed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+/** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = withPWA({
-  // next.js config
-  // reactStrictMode: true, 
-})
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  // i18n: {
+  //   locales: ["fr"],
+  //   defaultLocale: "fr",
+  // },
+});
+module.exports = nextConfig;
